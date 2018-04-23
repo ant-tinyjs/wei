28523c41c02b24d0938af81fd64f32fcf933d6f7
意绮 <fangjun.yfj@alibaba-inc.com>
2018-04-23 14:41:49 +0800

    update webpack

--- src/game/resources.js
+++ src/game/resources.js
@@ -1,5 +1,6 @@
-import 'res/frames/vendor/.tileset';
-import 'res/frames/hero/.tileset';
+// 打开注释即自动生成 tileset， 此功能需要安装 [ImageMagick](https://www.imagemagick.org/script/download.php)
+// import 'res/frames/vendor/.tileset';
+// import 'res/frames/hero/.tileset';
 
 const logoPNG = 'res/images/Tiny.js.png';
 const tilesetVendorJSON = 'res/images/tilesets/tileset-vendor.json';
--- webpack.config.js
+++ webpack.config.js
@@ -6,72 +6,84 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const tilesetDir = 'res/images/tilesets';
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.tileset/i,
-        use: {
-          loader: 'tinyjs-resource-loader',
-          options: {
-            process: true,
-            loader: 'none',
-            output: tilesetDir,
-            image: {
-              name: '[name].[ext]',
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development';
+
+  const config = {
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.tileset/i,
+          use: {
+            loader: 'tinyjs-resource-loader',
+            options: {
+              process: isDev,
+              loader: 'none',
+              output: tilesetDir,
+              image: {
+                name: '[name].[ext]',
+              },
+              json: {
+                name: '[name].[ext]',
+              },
             },
-            json: {
-              name: '[name].[ext]',
+          },
+        }, {
+          test: /\.(less|css)$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'less-loader',
+          ],
+        }, {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [ 'babel-preset-es2015', 'babel-preset-stage-0' ],
+              plugins: [ 'babel-plugin-transform-runtime' ],
             },
           },
         },
-      }, {
-        test: /\.(less|css)$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'less-loader',
-        ],
-      }, {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [ 'babel-preset-es2015', 'babel-preset-stage-0' ],
-            plugins: [ 'babel-plugin-transform-runtime' ],
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      'res': path.resolve('./res'),
+      ],
     },
-  },
-  plugins: [
-    new CleanWebpackPlugin([
+    resolve: {
+      alias: {
+        'res': path.resolve('./res'),
+      },
+    },
+    plugins: [
+      new CleanWebpackPlugin([
+        isDev ? tilesetDir : null,
+      ]),
+      new OptimizeCSSAssetsPlugin({}),
+      new MiniCssExtractPlugin({
+        filename: 'index.css',
+      }),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+      new CopyWebpackPlugin([ {
+        from: 'res/images',
+        to: 'res/images',
+      }, {
+        from: 'res/sounds',
+        to: 'res/sounds',
+      } ]),
+    ],
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  };
+
+  if (isDev) {
+    config.plugins.unshift(new CleanWebpackPlugin([
       tilesetDir,
-    ]),
-    new OptimizeCSSAssetsPlugin({}),
-    new MiniCssExtractPlugin({
-      filename: 'index.css',
-    }),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-    new CopyWebpackPlugin([ {
-      from: 'res/images',
-      to: 'res/images',
-    }, {
-      from: 'res/sounds',
-      to: 'res/sounds',
-    } ]),
-  ],
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    ]));
+  }
+
+  return config;
 };
